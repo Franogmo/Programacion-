a906d92de3d02caf7a672fffdfac436ebb0492ad
Franogmo <145434814+Franogmo@users.noreply.github.com>
2024-05-01 13:00:09 +0200

    Add files via upload

---
+++ ficheros1/ej2ficheros1v1.py
@@ -0,0 +1,18 @@
+"""
+Goal of the program: Displaying a prime number list from a text file.
+By Fran Ogallas
+Start date: 4th of April 2024. Last version: 4th of April 2024.
+"""
+
+
+def main():
+    prime_list = open("primedocument.txt", "rt")
+    current_line = prime_list.readline()
+    while current_line != "":
+        print(current_line, end="")
+        current_line = prime_list.readline()
+    prime_list.close()
+
+
+if __name__ == "__main__":
+    main()
